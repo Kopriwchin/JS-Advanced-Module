5705e6e122bdc191301f4a8ee1e9631d0a7dd4a8
Iliyan <ili_kop3@abv.bg>
2022-06-14 19:55:37 +0300

    Writing basic syntax of javascript

---
+++ js-introduction-syntax/demo.js
@@ -0,0 +1,22 @@
+//let firstName = 'georgi'
+//console.log(firstName);
+//output: georgi
+
+let firstName = 'Pesho';
+const age = 20;
+
+console.log(firstName + ' ' + age);
+// #Pesho 20
+
+firstName = 'Peter';
+
+console.log(firstName + ' ' + age);
+// #Peter 20
+
+if (true) {
+	let lastName = 'Petrov';
+	var testVar = 'Rado';
+}
+
+//console.log(lastName); -> #not defined
+console.log(testVar);
