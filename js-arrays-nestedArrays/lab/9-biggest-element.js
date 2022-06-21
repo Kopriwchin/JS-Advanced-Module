dd0ae08847cd77ca8bb7bd70da0edb9f1f99f2a4
Iliyan <ili_kop3@abv.bg>
2022-06-22 00:50:02 +0300

    Add another solution to 9th task

--- js-arrays-nestedArrays/lab/9-biggest-element.js
+++ js-arrays-nestedArrays/lab/9-biggest-element.js
@@ -3,12 +3,27 @@ function solve(array) {
 	console.log(result);
 }
 
+function solveAnotherWay(array) {
+	let biggestNum = Number.MIN_SAFE_INTEGER;
+
+	for (let i = 0; i < array.length; i++) {
+		for (let k = 0; k < array.length; k++) {
+			let currentNum = array[i][k];
+			if (currentNum > biggestNum) {
+				biggestNum = currentNum;
+			}
+		}
+	}
+
+	console.log(biggestNum);
+}
+
 solve([
 	[20, 50, 10],
 	[8, 33, 145]
 ]); // output: 145
 
-solve([
+solveAnotherWay([
 	[3, 5, 7, 12],
 	[-1, 4, 33, 2],
 	[8, 3, 0, 4]
