f661ba3cdfec27dbe0162f644c76246a99110798
Iliyan <ili_kop3@abv.bg>
2022-06-27 23:32:23 +0300

    Leaving the work here

---
+++ js-object-composition.js/lab/associative-array.js
@@ -0,0 +1,14 @@
+let phones = {
+	'Ivan Petrov': '1233211231',
+	'Georgi Georgiev': '1235551123',
+	'Peter Ivanov': '8273617234',
+}
+
+// Accessing and assigning
+console.log(phones['Ivan Petrov']);
+phones['Ivan Petrov'] = '359876551123';
+
+// Iteration
+for (key in phones) {
+	
+}
