d5cd8773b01b02314a5d1804ace49f7b3ed08060
Iliyan <ili_kop3@abv.bg>
2022-07-06 17:25:05 +0300

    Testing

--- js-object-composition/exercise-work/script.js
+++ js-object-composition/exercise-work/script.js
@@ -5,7 +5,32 @@ function objDemo(params) {
 		weight: 100
 	};
 
-	console.log(doubleChoco);
+	let keys = Object.keys(doubleChoco);
+	let values = Object.values(doubleChoco);
+
+	console.log(keys); // ['flourType', 'bonus', 'weight']
+	console.log(values); // ['brown', 'fruits', 100]
+
+	let entries = Object.entries(doubleChoco);
+	console.log(entries[0][0]); // flourType
+
+	let car = {
+		brand: 'Mercedes',
+		model: 'S-500',
+		yearOfProduction: 2017,
+		topSpeed: 300,
+		horsePowers: 435,
+		name: 'Peter',
+		driver: {
+			name: 'Ivan',
+			badgeName: `${this.name} Dimitrov` // this.name is undefined
+		},
+		fullName() {
+			return this.brand + ' ' + this.model + ' ' + this.yearOfProduction;
+		}
+	}
+
+	console.log(car.fullName());
 }
 
 objDemo();
