d6ffa72f2dd7ab34fc7e05b9849a41715b780930
Iliyan <ili_kop3@abv.bg>
2022-07-09 00:20:57 +0300

    Solved task

--- js-object-composition/exercise-work/7-towns-to-JSON.js
+++ js-object-composition/exercise-work/7-towns-to-JSON.js
@@ -1,21 +1,22 @@
 function solve(input) {
 	let towns = [];
 
-	for (const line of input) {
+	for (const line of input.slice(1)) {
 		let args = line.split(' | ');
+
 		let town = args[0].substring(2);
 		let latitude = Number(args[1]).toFixed(2);
-		console.log(latitude);
 		let longitude = Number(args[2].slice(0, -2)).toFixed(2);
-		console.log(longitude);
 
 		
 		let obj = {
 			Town: town,
-			Latitude: latitude,
-			Longitute: longitude
+			Latitude: Number(latitude),
+			Longitude: Number(longitude)
 		}
+		towns.push(obj);
 	}
+	console.log(JSON.stringify(towns));
 }
 
 solve(['| Town | Latitude | Longitude |',
