5e7189d0e123f8a724ffdf1e4371628f50347c8b
Iliyan <ili_kop3@abv.bg>
2022-07-14 15:35:50 +0300

    Done task 2

--- js-dom-I/exercise-work/02. Pascal or Camel Case/solution.js
+++ js-dom-I/exercise-work/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,30 @@
 function solve() {
-  //TODO...
+	let textElement = document.getElementById('text').value;
+	let namingConventionElement = document.getElementById('naming-convention');
+	let resultElement = document.getElementById('result');
+
+	const words = textElement.split(' ');
+
+	for (let i = 0; i < words.length; i++) {
+		words[i] = words[i].toLowerCase();
+	}
+
+	const finishedWord = [];
+	
+	if (namingConventionElement.value == 'Camel Case') {
+		finishedWord.push(words[0]);
+		for (let i = 1; i < words.length; i++) {
+			let finished = words[i][0].toUpperCase() + words[i].slice(1);
+			finishedWord.push(finished);
+		}
+		resultElement.innerText = finishedWord.join('');
+	} else if (namingConventionElement.value == 'Pascal Case') {
+		for (let i = 0; i < words.length; i++) {
+			let finished = words[i][0].toUpperCase() + words[i].slice(1);
+			finishedWord.push(finished);
+		}
+		resultElement.innerText = finishedWord.join('');
+	} else {
+		resultElement.innerText = 'Error!';
+	}
 }
