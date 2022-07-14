8e7a95a6cdd545672fa402e27376e7c6e7cbf50b
Iliyan <ili_kop3@abv.bg>
2022-07-14 16:26:27 +0300

    Done task 3

--- js-dom-I/exercise-work/03. Accordion/accordion.js
+++ js-dom-I/exercise-work/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonElement = document.querySelector('.button');
+
+	let textElement = document.getElementById('extra');
+	
+	if (buttonElement.innerHTML == 'More') {
+		buttonElement.innerHTML = 'Less';
+		textElement.style.display = 'block'
+	} else {
+		buttonElement.innerHTML = 'More';
+		textElement.style.display = 'none'
+	}
 }
