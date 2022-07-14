75a76dde8fed31b132b1de23a3872bcbf691fa81
Iliyan <ili_kop3@abv.bg>
2022-07-14 15:15:57 +0300

    Done first task

--- js-dom-I/exercise-work/01. Subtraction/subtract.js
+++ js-dom-I/exercise-work/01. Subtraction/subtract.js
@@ -1,3 +1,11 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumElement = document.getElementById('firstNumber');
+    let secondNumElement = document.getElementById('secondNumber');
+	let resultElement = document.getElementById('result');
+
+	let firstNumValue = Number(firstNumElement.value);
+	let secondNumValue = Number(secondNumElement.value);
+
+	let result = firstNumValue - secondNumValue;
+	resultElement.innerHTML = result;
 }
