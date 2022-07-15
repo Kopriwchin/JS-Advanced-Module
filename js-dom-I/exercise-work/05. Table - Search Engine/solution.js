9f12c6605e9ea11e6bb0161ead6950a1ff8ee3bf
Iliyan <ili_kop3@abv.bg>
2022-07-15 17:48:38 +0300

    Done task 6

--- js-dom-I/exercise-work/05. Table - Search Engine/solution.js
+++ js-dom-I/exercise-work/05. Table - Search Engine/solution.js
@@ -1,8 +1,25 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+	document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+	function onClick() {
+		let searchElement = document.querySelector('#searchField');
+		let searchInput = searchElement.value;
+		let table = document.querySelectorAll('.container tr');
 
-   }
+		// Clear everything from last search
+		table.forEach(x => x.classList.remove('select'));
+
+		// Skip description cells
+		for (let i = 2; i < table.length; i++) {
+			let tdElements = table[i].getElementsByTagName('td');
+			let tdElementsArray = Array.from(tdElements);
+			
+			let doesExistInWord = tdElementsArray.some(x => x.textContent.includes(searchInput));
+
+			if (doesExistInWord) {
+				table[i].classList.add('select');
+			}
+		}
+		searchElement.value = '';
+	}
 }
