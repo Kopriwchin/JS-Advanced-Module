bd991445c91d6ff24b95676c633d744323ac9c60
Iliyan <ili_kop3@abv.bg>
2022-07-15 22:42:14 +0300

    Done task 6

--- js-dom-I/exercise-work/06. Format the Text/solution.js
+++ js-dom-I/exercise-work/06. Format the Text/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-  //TODO
+	let inputValue = document.getElementById('input').value
+	let outputElement = document.getElementById('output');
+
+	let wholeTextArray = inputValue.split('.')
+		.filter(sentence => sentence.length > 0);
+
+	console.log(wholeTextArray);
+
+	for (let i = 0; i < wholeTextArray.length; i += 3) {
+		let arr = [];
+		for (let k = 0; k < 3; k++) {
+			// Check if undefined (outside of the array) elements are coming
+			if (wholeTextArray[i + k]) {
+				arr.push(wholeTextArray[i + k])
+			}
+		}
+		let paragraph = arr.join('. ') + '.';
+		outputElement.innerHTML += `<p>${paragraph}</p>`
+	}
 }
