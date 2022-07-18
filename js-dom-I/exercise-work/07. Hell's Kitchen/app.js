8179d021b6f9b35c3ca616660a1c7d1e4299942e
Kopriwchin <iliqn1234a@gmail.com>
2022-07-18 23:48:52 +0300

    Upload picture from given materials Initial set up for task

--- js-dom-I/exercise-work/07. Hell's Kitchen/app.js
+++ js-dom-I/exercise-work/07. Hell's Kitchen/app.js
@@ -2,6 +2,11 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      
+      let restaurant = {};
+      let restaurants = [];
+      let employeeWithSalary = {};
+
+      let inputElement = document.getElementsByTagName('textarea');
+      console.log(inputElement);
    }
 }
