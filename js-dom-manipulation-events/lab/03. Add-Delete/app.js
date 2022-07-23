d9ecc9148cce5ed75197d5d12785d0555666f390
Kopriwchin <iliqn1234a@gmail.com>
2022-07-23 19:00:59 +0300

    Done task 3

--- js-dom-manipulation-events/lab/03. Add-Delete/app.js
+++ js-dom-manipulation-events/lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let itemsElement = document.getElementById('items');
+    let inputElement = document.getElementById('newItemText');
+
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+
+    inputElement.value = '';
+
+    let deleteElement = document.createElement('a');
+    deleteElement.href = '#';
+    deleteElement.textContent = '[Delete]';
+    deleteElement.addEventListener('click', (e) => {
+        e.currentTarget.parentNode.remove();
+    });
+    
+    liElement.appendChild(deleteElement);
+    itemsElement.appendChild(liElement);
 }
