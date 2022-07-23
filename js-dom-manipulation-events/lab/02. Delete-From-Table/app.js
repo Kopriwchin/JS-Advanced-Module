82c29af5d2658f5ab30d084bbd73f27167f5989b
Kopriwchin <iliqn1234a@gmail.com>
2022-07-23 19:00:53 +0300

    Done task 2

--- js-dom-manipulation-events/lab/02. Delete-From-Table/app.js
+++ js-dom-manipulation-events/lab/02. Delete-From-Table/app.js
@@ -1,3 +1,14 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let customerElements = document.querySelectorAll('tbody tr td:nth-of-type(2)');
+    let inputElement = document.querySelector('input[name="email"]');
+    let resultElement = document.getElementById('result');
+
+    for (const customer of customerElements) {
+        if (inputElement.value == customer.textContent) {
+            customer.parentElement.remove();
+            resultElement.textContent = 'Deleted.';
+        } else {
+            resultElement.textContent = 'Not found.';
+        }
+    }
 }
