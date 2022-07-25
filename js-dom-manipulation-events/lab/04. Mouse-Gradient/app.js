eeabd5a825fd7c14e4bd40df4a169fa1cdcddbce
Kopriwchin <iliqn1234a@gmail.com>
2022-07-25 23:53:36 +0300

    Done task 4

--- js-dom-manipulation-events/lab/04. Mouse-Gradient/app.js
+++ js-dom-manipulation-events/lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,15 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
-}
+    let gradient = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+    function gradientMove(event) {
+      let power = event.offsetX / (event.target.clientWidth - 1);
+      power = Math.trunc(power * 100);
+      resultElement.textContent = power + "%";
+    }
+    function gradientOut() {
+      resultElement.textContent = "";
+    }
+  };
+  
