d49d4ed1eb5d5224ac99c03fa34992af6bc2496d
Kopriwchin <iliqn1234a@gmail.com>
2022-07-28 03:58:25 +0300

    Done task 6

--- js-dom-manipulation-events/lab/06. Dynamic-Validation/app.js
+++ js-dom-manipulation-events/lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    let reg = /^([\w\-.]+)@([a-z]+)(\.[a-z]+)+$/;
+
+    inputElement.addEventListener('change', 
+        function checkEmail(event) {
+            if (reg.test(event.target.value)) {
+                event.target.removeAttribute('class');
+                return;
+            }
+            event.target.className = 'error';
+        }
+    );
 }
