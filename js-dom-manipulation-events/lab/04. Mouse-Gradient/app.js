67a05a12038b19f6d4f5e2fb184eae745996f482
Kopriwchin <iliqn1234a@gmail.com>
2022-07-28 03:25:23 +0300

    Done task 5

--- js-dom-manipulation-events/lab/04. Mouse-Gradient/app.js
+++ js-dom-manipulation-events/lab/04. Mouse-Gradient/app.js
@@ -4,12 +4,11 @@ function attachGradientEvents() {
     gradient.addEventListener('mousemove', gradientMove);
     gradient.addEventListener('mouseout', gradientOut);
     function gradientMove(event) {
-      let power = event.offsetX / (event.target.clientWidth - 1);
-      power = Math.trunc(power * 100);
-      resultElement.textContent = power + "%";
+        let power = event.offsetX / (event.target.clientWidth - 1);
+        power = Math.trunc(power * 100);
+        resultElement.textContent = power + "%";
     }
     function gradientOut() {
-      resultElement.textContent = "";
+        resultElement.textContent = "";
     }
-  };
-  
+};
--- js-dom-manipulation-events/lab/05. Highlight-Active/app.js
+++ js-dom-manipulation-events/lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    let sectionElements = document.querySelectorAll('input');
+    let sectionElementsArray = Array.from(sectionElements);
+    
+    sectionElementsArray.forEach(x => {
+        x.addEventListener('focus', function() {
+            x.parentElement.classList.add('focused');
+        })
+    })
+
+    sectionElementsArray.forEach(x => {
+        x.addEventListener('blur', function() {
+            x.parentElement.classList.remove('focused');
+        })
+    })
 }
