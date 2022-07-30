98d5bf326874ae42f288231b1c2bf29ffa397dfa
Iliyan <ili_kop3@abv.bg>
2022-07-30 23:32:20 +0300

    Done task 1

--- js-dom-manipulation-events/exercise-work/01. Sections/script.js
+++ js-dom-manipulation-events/exercise-work/01. Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   console.log('TODO:...');
-}
+	let wordsArray = Array.from(words);
+	console.log(wordsArray);
+	let contentElement = document.querySelector('#content');
+
+	for (let i = 0; i < wordsArray.length; i++) {
+		let customDiv = document.createElement('div');
+		let customParagraph = document.createElement('p');
+		customParagraph.textContent = `${wordsArray[i]}`
+		customParagraph.style.display = 'none'
+		customDiv.appendChild(customParagraph);
+
+		customDiv.addEventListener('click', function() {
+			customParagraph.style.display = 'block';
+		})
+
+		contentElement.appendChild(customDiv);
+	}
+} 
