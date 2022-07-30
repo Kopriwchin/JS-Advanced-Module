60b5598e686465a8b564fce6ecf9f6baeec7762e
Iliyan <ili_kop3@abv.bg>
2022-07-30 17:58:44 +0300

    Done task 7

--- js-dom-manipulation-events/lab/07. Shopping-Cart/solution.js
+++ js-dom-manipulation-events/lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-   //TODO...
+	let finalPrice = 0;
+	let productsAdded = [];
+
+	let addButtons = document.getElementsByClassName('add-product');
+	let addButtonsArray = Array.from(addButtons);
+	let resultElement = document.getElementsByTagName('textarea')[0];
+	let checkoutButtonElement = document.getElementsByClassName('checkout')[0];
+
+	for (let i = 0; i < addButtonsArray.length; i++) {
+		let nameProduct = document.querySelector(`body > div > div:nth-child(${i + 2}) > div.product-details > div.product-title`).textContent;
+		let priceProduct = document.querySelector(`body > div > div:nth-child(${i + 2}) > div.product-line-price`).textContent;
+
+		addButtons[i].addEventListener('click', function() {
+			if (!productsAdded.includes(nameProduct)) {
+				productsAdded.push(nameProduct);
+			}
+			resultElement.textContent += `Added ${nameProduct} for ${priceProduct} to the cart.\n`;
+			finalPrice += Number(priceProduct);
+		});
+	}
+
+	checkoutButtonElement.addEventListener('click', function() {
+		resultElement.textContent += `You bought ${productsAdded.join(', ')} for ${finalPrice.toFixed(2)}.`
+		let allButtonsArray = Array.from(document.querySelectorAll('button'));
+		allButtonsArray.forEach(x => x.disabled = true);
+	})
+	
 }
