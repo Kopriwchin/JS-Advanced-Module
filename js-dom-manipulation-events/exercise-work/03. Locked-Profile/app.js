57b26a273dabbfd5c4a356eeea60db3c5523016d
Iliyan <ili_kop3@abv.bg>
2022-07-31 15:44:19 +0300

    Done task 3

--- js-dom-manipulation-events/exercise-work/03. Locked-Profile/app.js
+++ js-dom-manipulation-events/exercise-work/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+	let showButtons = document.querySelectorAll('button');
+	let showButtonsArray = Array.from(showButtons);
+
+	showButtonsArray.forEach(x => {
+		x.addEventListener('click', function(e) {
+			let radioButtonLocked = e.target.parentNode.querySelector(`input`);
+			let hiddenField = e.target.parentNode.querySelector('div');
+			if (!radioButtonLocked.checked) {
+				if (x.textContent === 'Show more') {
+					hiddenField.style.display = 'block';
+					x.textContent = 'Hide it';
+				} else if (x.textContent === 'Hide it') {
+					hiddenField.style.display = 'none';
+					x.textContent = 'Show more';
+				}
+			}
+		})
+	})
 }
