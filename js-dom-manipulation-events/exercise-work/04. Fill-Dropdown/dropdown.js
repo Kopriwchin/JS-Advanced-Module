929c16f28670ef2feb5f5bfb9be9200d1edd312d
Iliyan <ili_kop3@abv.bg>
2022-07-31 16:06:08 +0300

    Done task 4

--- js-dom-manipulation-events/exercise-work/04. Fill-Dropdown/dropdown.js
+++ js-dom-manipulation-events/exercise-work/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+	let menuElement = document.getElementById('menu');
+    let textElement = document.getElementById('newItemText');
+	let valueElement = document.getElementById('newItemValue');
+
+	let optionElement = document.createElement('option');
+	optionElement.text = textElement.value;
+	optionElement.value = valueElement.value;
+
+	menuElement.appendChild(optionElement);
+	textElement.value = '';
+	valueElement.value = '';
 }
