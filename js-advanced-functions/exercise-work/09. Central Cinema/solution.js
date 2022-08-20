30a523b6c518919c5596c032e25e1ec5e97a0787
Kopriwchin <iliqn1234a@gmail.com>
2022-08-20 16:08:35 +0300

    Done task 9

--- js-advanced-functions/exercise-work/09. Central Cinema/solution.js
+++ js-advanced-functions/exercise-work/09. Central Cinema/solution.js
@@ -2,37 +2,73 @@ function solve() {
     let nameInputElement = document.querySelector('input[placeholder="Name"]');
     let hallInputElement = document.querySelector('input[placeholder="Hall"]');
     let ticketPriceInputElement = document.querySelector('input[placeholder="Ticket Price"]');
-    let ulElement = document.querySelector('#movies > ul');
+    let moviesUlElement = document.querySelector('#movies > ul');
+    let archieveUlElement = document.querySelector('#archive > ul');
+    let clearButtonElement = document.querySelector('#archive > button');
+    let onScreenButtonElement = document.querySelector('#container button');
 
-    let onScreenButtonElement = document.querySelector('button');
     onScreenButtonElement.addEventListener('click', addMovie);
+    clearButtonElement.addEventListener('click', clearArchive)
 
     function addMovie(e) {
         e.preventDefault();
 
-        if (nameInputElement.value &&
-            hallInputElement.value &&
-            (!isNaN(ticketPriceInputElement.value) && ticketPriceInputElement.value != '')) { 
-                ulElement.innerHTML +=
+        if (nameInputElement.value.trim() &&
+            hallInputElement.value.trim() &&
+            (!isNaN(ticketPriceInputElement.value.trim()) && ticketPriceInputElement.value.trim() != '')) {
+            moviesUlElement.innerHTML +=
                 `<li>
                     <span>${nameInputElement.value}</span>
                     <strong>Hall: ${hallInputElement.value}</strong>
                     <div>
-                      <strong>${ticketPriceInputElement.value}</strong>
+                      <strong>${Number(ticketPriceInputElement.value).toFixed(2)}</strong>
                       <input placeholder="Tickets Sold">
                       <button>Archive</button>
                     </div>
                 </li>`
- 
-            nameInputElement.value = null;
-            hallInputElement.value = null;
-            ticketPriceInputElement.value = null;
+
+            clearInputs();
         }
+
+        clearInputs();
+        let archiveButtons = document.querySelectorAll('ul button');
+
+        archiveButtons.forEach(x => {
+            x.addEventListener('click', archieveMovie)
+        })
     }
 
-    
-    function archieveMovie() {
+    function archieveMovie(e) {
+        let liElement = e.currentTarget.parentNode.parentNode;
+        let ticketsSoldValue = e.currentTarget.parentNode.querySelector('input').value.trim();
+        let overallPrice = Number(ticketsSoldValue) * Number(liElement.querySelector('div > strong').textContent);
 
+        if (!isNaN(ticketsSoldValue) && ticketsSoldValue != '') {
+            archieveUlElement.innerHTML +=
+                `<li>
+                <span>${liElement.querySelector('span').textContent}</span>
+                <strong>Total amount: ${overallPrice.toFixed(2)}</strong>
+                <button>Delete</button>
+                </li>`
+
+            liElement.remove();
+        }
+
+
+        let deleteButtons = document.querySelectorAll('#archive > ul button');
+        deleteButtons.forEach(x => {
+            x.addEventListener('click', deleteMovie);
+        })
+    }
+
+    function deleteMovie(e) {
+        e.currentTarget.parentNode.remove();
+    }
+
+    function clearArchive(e) {
+        e.currentTarget.parentNode.querySelectorAll('ul > li').forEach(x => {
+            x.remove();
+        })
     }
 
     function clearInputs() {
