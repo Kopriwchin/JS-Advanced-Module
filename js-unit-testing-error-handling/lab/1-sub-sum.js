429b12e87f8f858ec9989aa6831f323d1ff9392b
Kopriwchin <iliqn1234a@gmail.com>
2022-08-21 19:09:19 +0300

    Add unit test lab work

--- js-unit-testing-error-handling/lab/1-sub-sum.js
+++ js-unit-testing-error-handling/lab/1-sub-sum.js
@@ -22,8 +22,11 @@ function solve(array, startIndex, endIndex) {
     return sum;
 }
 
-solve([10, 20, 30, 40, 50, 60], 3, 300); // 150
-solve([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1); // 3.3
-solve([10, 'twenty', 30, 40], 0, 2); // NaN
-solve([], 1, 2); // 0
-solve('text', 0, 2); // NaN
+module.exports = solve;
+
+
+// solve([10, 20, 30, 40, 50, 60], 3, 300); // 150
+// solve([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1); // 3.3
+// solve([10, 'twenty', 30, 40], 0, 2); // NaN
+// solve([], 1, 2); // 0
+// solve('text', 0, 2); // NaN
---
+++ js-unit-testing-error-handling/lab/calc-module.js
@@ -0,0 +1,15 @@
+function calc(a, b) {
+    return a + b;
+};
+
+function print(name) {
+    console.log(name);
+}
+
+function testFunc(aaa) {
+    console.log(aaa);
+}
+
+module.exports = calc;
+module.exports = print;
+module.exports = testFunc;
