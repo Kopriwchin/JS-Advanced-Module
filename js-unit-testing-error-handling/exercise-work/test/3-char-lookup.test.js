900665f6c76df565c89b2e4240fb77b3aef34946
Kopriwchin <iliqn1234a@gmail.com>
2022-08-30 00:09:02 +0300

    Add task 3 template

---
+++ js-unit-testing-error-handling/exercise-work/test/3-char-lookup.test.js
@@ -0,0 +1,8 @@
+const assert = require('mocha').assert;
+const lookupChar = require('../3-char-lookup');
+
+describe('Function that retrieves a character at a given index from a passed-in string', () => {
+    it('Should check if the first parameter is string', () => {
+        
+    });
+})
