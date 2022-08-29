dbf61fe1f921d7557661bc998832b562a7a28650
Kopriwchin <iliqn1234a@gmail.com>
2022-08-29 23:40:56 +0300

    Readd first exercise, otherwise 'npm test' doesn't work

--- js-unit-testing-error-handling/exercise-work/1-request-validator.js
+++ js-unit-testing-error-handling/exercise-work/1-request-validator.js
@@ -32,16 +32,24 @@ function requestValidator(input) {
     return input;
 }
 
-requestValidator({
-    method: 'GET',
-    uri: 'svn.public.catalog',
-    version: 'HTTP/1.1',
-    message: ''
-});
+module.exports = requestValidator;
 
-requestValidator({
-    method: 'OPTIONS',
-    uri: 'git.master',
-    version: 'HTTP/1.1',
-    message: '-recursive'
-});
+// requestValidator({
+//     method: 'GET',
+//     uri: 'svn.public.catalog',
+//     version: 'HTTP/1.1',
+//     message: ''
+// });
+
+// requestValidator({
+//     method: 'OPTIONS',
+//     uri: 'git.master',
+//     version: 'HTTP/1.1',
+//     message: '-recursive'
+// });
+
+// requestValidator({
+//     method: 'POST',
+//     uri: 'home.bash',
+//     message: 'rm -rf /*'
+// });
