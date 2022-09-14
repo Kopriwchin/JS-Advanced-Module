3133b1a577417ad20b13ec686e58addd9d08f2c4
Kopriwchin <iliqn1234a@gmail.com>
2022-09-14 14:54:13 +0300

    Add 70% ready task 2

---
+++ js-prototypes-inheritance/exercise-work/2-string-extension.js
@@ -0,0 +1,69 @@
+(function solve() {
+    String.prototype.ensureStart = function (str) {
+        if (!this.startsWith(str)) {
+            return str + this;
+        } else {
+            return this;
+        }
+    }
+
+    String.prototype.ensureEnd = function (str) {
+        if (!this.endsWith(str)) {
+            return this + str;
+        }
+        return this;
+    }
+
+    String.prototype.isEmpty = function (str) {
+        return this.length == 0 ? true : false;
+    }
+
+    String.prototype.truncate = function (n) {
+        if (this.length < n) {
+            return this;
+        } else {
+            let splitString = this.split('');
+            let indexes = splitString.reduce(function (a, e, i) {
+                if (e === ' ')
+                    a.push(i);
+                return a;
+            }, []).reverse();
+
+            if (indexes.length == 0) {
+                return this.split(0, n - 3) + '...';
+            } else if (n < 4) {
+                let string = '.';
+                for (let i = 1; i < n; i++) {
+                    string += '.';
+                }
+                return string;
+            }
+            else {
+                for (let i = 0; i < indexes.length; i++) {
+                    console.log(indexes[i]);
+                    let initString = this.slice(0, indexes[i]) + '...';
+                    if (initString.length < n) {
+                        return initString;
+                    }
+                }
+                return this.slice(0, indexes[0] + '...');
+            }
+        }
+    }
+
+    String.prototype.format = function (string, ...params) {
+
+    }
+})();
+
+let str = 'my string';
+str = str.ensureStart('my');
+str = str.ensureStart('hello ');
+// console.log(str.truncate(16));
+// console.log(str.truncate(14));
+console.log(str.truncate(4));
+console.log(str.truncate(2));
+// str = String.format('The {0} {1} fox',
+//     'quick', 'brown');
+// str = String.format('jumps {0} {1}',
+//     'dog');
