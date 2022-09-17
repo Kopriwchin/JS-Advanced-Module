30cd031d13f63c81b0e2c72fa3772c0001ff0704
Kopriwchin <iliqn1234a@gmail.com>
2022-09-17 22:40:31 +0300

    Add task 2

--- js-prototypes-inheritance/exercise-work/2-string-extension.js
+++ js-prototypes-inheritance/exercise-work/2-string-extension.js
@@ -19,40 +19,21 @@
     }
 
     String.prototype.truncate = function (n) {
-        if (this.length < n) {
-            return this;
-        } else {
-            let splitString = this.split('');
-            let indexes = splitString.reduce(function (a, e, i) {
-                if (e === ' ')
-                    a.push(i);
-                return a;
-            }, []).reverse();
-
-            if (indexes.length == 0) {
-                return this.split(0, n - 3) + '...';
-            } else if (n < 4) {
-                let string = '.';
-                for (let i = 1; i < n; i++) {
-                    string += '.';
-                }
-                return string;
+        let lastSpace = str.lastIndexOf(' ');
+        let result;
+        if(n > str.length){
+            return str;
+        } else if(n<str.length && lastSpace != -1){
+            if(lastSpace <= n && n < str.length){
+                result = str.slice(0,lastSpace)+ '...';
+                return result;
             }
-            // string.truncate(4);
-            // hello my string
-            // h....
-            else if (n < splitString.indexOf(' ')) {
-                
-            }
-            else {
-                for (let i = 0; i < indexes.length; i++) {
-                    let initString = this.slice(0, indexes[i]) + '...';
-                    if (initString.length <= n) {
-                        return initString;
-                    }
-                }
-                return this.slice(0, indexes[0] + '...');
-            }
+        } else if(lastSpace === -1 && n>= 4){
+            let end = n-3;
+            let temp = str.slice(0, end);
+            return temp + '...' ;
+        } else if(n<4){
+            return '.'.repeat(n);
         }
     }
 
@@ -64,18 +45,3 @@
         return string;
     }
 })();
-
-let str = 'my string';
-// str = str.ensureStart('my');
-// str = str.ensureStart('hello ');
-// console.log(str.truncate(16));
-// console.log(str.truncate(14));
-// console.log(str.truncate(8));
-
-console.log(str.truncate(4));
-console.log(str.truncate(2));
-str = String.format('The {0} {1} fox',
-    'quick', 'brown');
-str = String.format('jumps {0} {1}',
-    'dog');
-
