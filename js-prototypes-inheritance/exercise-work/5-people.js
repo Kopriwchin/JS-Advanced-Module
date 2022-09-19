2c48f0cb59d1fb11042afd7421a7099ec34b056d
Kopriwchin <iliqn1234a@gmail.com>
2022-09-19 15:58:29 +0300

    Deleted unwanted spacings

--- js-prototypes-inheritance/exercise-work/5-people.js
+++ js-prototypes-inheritance/exercise-work/5-people.js
@@ -7,7 +7,7 @@ function solution() {
             this.tasks = [];
             this.taskCount = 0;
         }
-    
+
         work() {
             if (this.taskCount == this.tasks.length) {
                 this.taskCount = 0;
@@ -15,7 +15,7 @@ function solution() {
             console.log(this.tasks[this.taskCount]);
             this.taskCount++;
         }
-    
+
         collectSalary() {
             if (this.dividend) {
                 console.log(`${this.name} received ${this.salary + this.dividend} this month.`);
@@ -24,14 +24,14 @@ function solution() {
             console.log(`${this.name} received ${this.salary} this month.`);
         }
     }
-    
+
     class Junior extends Employee {
         constructor(name, age) {
             super(name, age);
             this.tasks = [`${this.name} is working on a simple task.`];
         }
     }
-    
+
     class Senior extends Employee {
         constructor(name, age) {
             super(name, age);
@@ -42,7 +42,7 @@ function solution() {
             ];
         }
     }
-    
+
     class Manager extends Employee {
         constructor(name, age) {
             super(name, age);
@@ -57,30 +57,27 @@ function solution() {
     return {Employee, Junior, Senior, Manager}
 }
 
+const junior = new Junior('Ivan',25);
 
+junior.work();
+junior.work();
 
-const junior = new Junior('Ivan',25); 
- 
-junior.work();  
-junior.work();  
- 
-junior.salary = 5811; 
-junior.collectSalary();  
- 
-const senior = new Senior('Alex', 31); 
- 
-senior.work();  
-senior.work();  
-senior.work();  
-senior.work();  
- 
-senior.salary = 12050; 
-senior.collectSalary();  
- 
-const manager = new Manager('Tom', 55); 
- 
-manager.salary = 15000; 
-manager.collectSalary();  
-manager.dividend = 2500; 
-manager.collectSalary();  
+junior.salary = 5811;
+junior.collectSalary();
 
+const senior = new Senior('Alex', 31);
+
+senior.work();
+senior.work();
+senior.work();
+senior.work();
+ 
+senior.salary = 12050;
+senior.collectSalary();
+ 
+const manager = new Manager('Tom', 55);
+ 
+manager.salary = 15000;
+manager.collectSalary();
+manager.dividend = 2500;
+manager.collectSalary();
