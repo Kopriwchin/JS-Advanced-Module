f366178798e06537c4f4dbbcbdf5b87e22427380
Kopriwchin <iliqn1234a@gmail.com>
2022-09-19 12:23:42 +0300

    Add task with max scoring

--- js-prototypes-inheritance/exercise-work/5-people.js
+++ js-prototypes-inheritance/exercise-work/5-people.js
@@ -1,23 +1,62 @@
-class Employee {
-    constructor(name, age) {
-        this.name = name;
-        this.age = Number(age);
-        this.salary = 0;
-        this.tasks = [];
+function solution() {
+    class Employee {
+        constructor(name, age) {
+            this.name = name;
+            this.age = Number(age);
+            this.salary = 0;
+            this.tasks = [];
+            this.taskCount = 0;
+        }
+    
+        work() {
+            if (this.taskCount == this.tasks.length) {
+                this.taskCount = 0;
+            }
+            console.log(this.tasks[this.taskCount]);
+            this.taskCount++;
+        }
+    
+        collectSalary() {
+            if (this.dividend) {
+                console.log(`${this.name} received ${this.salary + this.dividend} this month.`);
+                return;
+            }
+            console.log(`${this.name} received ${this.salary} this month.`);
+        }
+    }
+    
+    class Junior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks = [`${this.name} is working on a simple task.`];
+        }
+    }
+    
+    class Senior extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.tasks = [
+                `${this.name} is working on a complicated task.`,
+                `${this.name} is taking time off work.`,
+                `${this.name} is supervising junior workers.`,
+            ];
+        }
+    }
+    
+    class Manager extends Employee {
+        constructor(name, age) {
+            super(name, age);
+            this.dividend = 0;
+            this.tasks = [
+                `${this.name} scheduled a meeting.`,
+                `${this.name} is preparing a quarterly report.`,
+            ]
+        }
     }
 
-    work() {
-
-    }
-
-    collectSalary() {
-
-    }
+    return {Employee, Junior, Senior, Manager}
 }
 
-class Junior extends Employee {
-    constructor()
-}
 
 
 const junior = new Junior('Ivan',25); 
