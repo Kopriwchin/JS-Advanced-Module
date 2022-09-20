6a98f387f7bd5a66d74a400b494899ad2ec6ecf0
Kopriwchin <iliqn1234a@gmail.com>
2022-09-20 23:03:03 +0300

    Add half finished task 1 from June 25th Exam

---
+++ exam-preparation/25-june-2022/01. Car Dealers/app.js
@@ -0,0 +1,50 @@
+window.addEventListener("load", solve);
+
+function solve() {
+  let publishButton = document.getElementById('publish');
+  let makeInputElement = document.getElementById('make');
+  let modelInputElement = document.getElementById('model');
+  let yearInputElement = document.getElementById('year');
+  let fuelElement = document.getElementById('fuel');
+
+  let originalCostInput = document.getElementById('original-cost');
+  let sellingPriceInput = document.getElementById('selling-price');
+
+
+
+  publishButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (makeInputElement.value.length == 0 || modelInputElement.value.length == 0 ||
+      yearInputElement.value.length == 0 || originalCostInput.value.length == 0 ||
+      sellingPriceInput.value.length == 0 || (Number(originalCostInput.value) > Number(sellingPriceInput.value))
+    ) {
+      return;
+    }
+    else {
+      let tableBody = document.getElementById('table-body');
+      tableBody.innerHTML +=
+        `
+        <tr class="row">
+          <td>${makeInputElement.value}</td>
+          <td>${modelInputElement.value}</td>
+          <td>${yearInputElement.value}</td>
+          <td>${fuelElement.value}</td>
+          <td>${originalCostInput.value}</td>
+          <td>${sellingPriceInput.value}</td>
+          <td>
+            <button class="action-btn edit">Edit</button>
+            <button class="action-btn sell">Sell</button>
+          </td>
+        </tr>`;
+
+        makeInputElement.value = '';
+        modelInputElement.value = '';
+        yearInputElement.value = '';
+        fuelElement.value = '';
+        originalCostInput.value = '';
+        sellingPriceInput.value = '';
+
+    };
+  })
+}
