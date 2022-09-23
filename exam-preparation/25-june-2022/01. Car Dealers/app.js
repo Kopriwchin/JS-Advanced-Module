460927360532655ef633443a51cc528996b09e10
Kopriwchin <iliqn1234a@gmail.com>
2022-09-23 21:26:03 +0300

    Make changes to task 1

--- exam-preparation/25-june-2022/01. Car Dealers/app.js
+++ exam-preparation/25-june-2022/01. Car Dealers/app.js
@@ -2,47 +2,98 @@ window.addEventListener("load", solve);
 
 function solve() {
   let publishButton = document.getElementById('publish');
+
   let makeInputElement = document.getElementById('make');
   let modelInputElement = document.getElementById('model');
   let yearInputElement = document.getElementById('year');
   let fuelElement = document.getElementById('fuel');
-
   let originalCostInput = document.getElementById('original-cost');
   let sellingPriceInput = document.getElementById('selling-price');
+  let tableBody = document.getElementById('table-body');
+  const soldCarsElement = document.getElementById("cars-list");
+  const totalProfitElement = document.getElementById("profit");
+  let profitMade = 0;
 
   publishButton.addEventListener('click', function (e) {
     e.preventDefault();
 
     if (makeInputElement.value.length == 0 || modelInputElement.value.length == 0 ||
       yearInputElement.value.length == 0 || originalCostInput.value.length == 0 ||
-      sellingPriceInput.value.length == 0 || (Number(originalCostInput.value) >= Number(sellingPriceInput.value))
+      sellingPriceInput.value.length == 0 || (Number(originalCostInput.value) > Number(sellingPriceInput.value))
     ) {
       return;
     }
     else {
-      let tableBody = document.getElementById('table-body');
-      tableBody.innerHTML +=
-        `
-        <tr class="row">
-          <td>${makeInputElement.value}</td>
-          <td>${modelInputElement.value}</td>
-          <td>${yearInputElement.value}</td>
-          <td>${fuelElement.value}</td>
-          <td>${originalCostInput.value}</td>
-          <td>${sellingPriceInput.value}</td>
-          <td>
-            <button class="action-btn edit">Edit</button>
-            <button class="action-btn sell">Sell</button>
-          </td>
-        </tr>`;
-        console.log(document.querySelectorAll('#table-body > tr > td > button[class="action-btn edit"]'));
-        clearInputs()
+      const tr = elGenerator('tr');
+      tr.setAttribute('class', 'row');
+
+      elGenerator('td', `${makeInputElement.value}`, tr);
+      elGenerator('td', `${modelInputElement.value}`, tr);
+      elGenerator('td', `${yearInputElement.value}`, tr);
+      elGenerator('td', `${fuelElement.value}`, tr);
+      elGenerator('td', `${originalCostInput.value}`, tr);
+      elGenerator('td', `${sellingPriceInput.value}`, tr);
+      const actionCell = elGenerator('td');
+      tr.appendChild(actionCell);
+
+      const editBtn = elGenerator("button", "Edit");
+      editBtn.setAttribute("class", "action-btn");
+      editBtn.setAttribute("id", "edit");
+      actionCell.appendChild(editBtn);
+
+      const sellBtn = elGenerator("button", "Sell");
+      sellBtn.setAttribute("class", "action-btn");
+      sellBtn.setAttribute("id", "sell");
+      actionCell.appendChild(sellBtn);
+
+      tableBody.appendChild(tr);
+
+      console.log(makeInputElement.value);
+      editBtn.addEventListener("click", (ev) =>
+        editOffer(
+          ev,
+          makeInputElement.value,
+          modelInputElement.value,
+          yearInputElement.value,
+          fuelElement.value,
+          originalCostInput.value,
+          sellingPriceInput.value
+        )
+      );
+
+      sellBtn.addEventListener("click", (ev) =>
+        sellCar(
+          ev,
+          makeInputEl,
+          modelInputEl,
+          yearInputEl,
+          firstPriceInputEl,
+          sellingPriceInputEl
+        )
+      );
+
+      clearInputs();
     };
   });
 
-  document.querySelectorAll('#table-body > tr > td > button[class="action-btn edit"]').forEach(x => x.addEventListener('click', function(e) {
-    console.log(555)
-  }));
+  function editOffer(
+    ev,
+    _makeInputEl,
+    _modelInputEl,
+    _yearInputEl,
+    _fuelInputEl,
+    _firstPriceInputEl,
+    _sellingPriceInputEl
+  ) {
+    ev.target.parentNode.parentNode.remove();
+
+    makeInputElement.value = _makeInputEl;
+    modelInputElement.value = _modelInputEl;
+    yearInputElement.value = _yearInputEl;
+    fuelElement.value = _fuelInputEl;
+    originalCostInput.value = _firstPriceInputEl;
+    sellingPriceInput.value = _sellingPriceInputEl;
+  }
 
   function clearInputs() {
     makeInputElement.value = '';
@@ -51,5 +102,16 @@ function solve() {
     fuelElement.value = '';
     originalCostInput.value = '';
     sellingPriceInput.value = '';
+  };
+
+  function elGenerator(type, content, parent) {
+    const element = document.createElement(type);
+    element.textContent = content;
+
+    if (parent) {
+      parent.appendChild(element);
+    }
+
+    return element;
   }
 }
