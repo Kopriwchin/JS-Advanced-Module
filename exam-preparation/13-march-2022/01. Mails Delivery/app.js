80659f123aecc370909c67501bd7c18814dca5f7
Kopriwchin <iliqn1234a@gmail.com>
2022-10-06 21:51:06 +0300

    Fix bugs and fully do task 1

--- exam-preparation/13-march-2022/01. Mails Delivery/app.js
+++ exam-preparation/13-march-2022/01. Mails Delivery/app.js
@@ -1,10 +1,11 @@
 function solve() {
     let addListButtonElement = document.getElementById('add');
     let resetButtonElement = document.getElementById('reset');
-    
+
     let recipentNameElement = document.getElementById('recipientName');
     let titleElement = document.getElementById('title');
     let textArea = document.getElementById('message');
+    let deleteListEl = document.querySelector('ul.delete-list');
 
     let listMailsElement = document.getElementById('list');
 
@@ -15,11 +16,11 @@ function solve() {
 
     addListButtonElement.addEventListener('click', function (e) {
         e.preventDefault();
-       
+
         if (recipentNameElement.value.length == 0 || titleElement.value.length == 0 || textArea.value.length == 0) {
             return;
         }
-        
+
         let liElement = generateElement('li', '', listMailsElement);
         let h4Element = generateElement('h4', `Title: ${titleElement.value}`, liElement);
         let anotherH4Element = generateElement('h4', `Recipient Name: ${recipentNameElement.value}`, liElement);
@@ -35,41 +36,54 @@ function solve() {
         deleteButtonElement.type = 'submit';
         deleteButtonElement.id = 'delete';
 
-        let sendButtons = document.querySelectorAll('#send');
-        let deleteButtons = document.querySelectorAll('#delete');
+        deleteButtonElement.addEventListener('click', function (e) {
+            let values = e.currentTarget.parentNode.parentNode.querySelectorAll('h4');;
+            let title = values[0].textContent.split(' ')[1];
+            let receiver = values[1].textContent.split(' ')[2];
+            
+            let liDeleteEl = generateElement('li');
+            let receiverSpanEl = generateElement('span', `To: ${receiver}`, liDeleteEl);
+            let titleSpanEl = generateElement('span', `Title: ${title}`, liDeleteEl);
 
-        deleteButtons.forEach(x => x.addEventListener('click', function(e) {
-            deleteMail(e)
-        }));
-        
-        sendButtons.forEach(x => x.addEventListener('click', function(e) {
-            {
-                let ulSentEmailsElement = document.getElementsByClassName('sent-list')[0];
+            e.currentTarget.parentNode.parentNode.remove();
+            deleteListEl.appendChild(liDeleteEl);
+        });
 
-                let liEl = generateElement('li', '', ulSentEmailsElement);
-                let data = e.currentTarget.parentNode.parentNode.querySelectorAll('h4');
-                let title = data[0].textContent.split(' ')[1];
-                let receiver = data[1].textContent.split(' ')[2];
-                
-                let spanEl = generateElement('span', `To: ${receiver}`, liEl);
-                let secondSpanEl = generateElement('span', `Title: ${title}`, liEl);
+        sendButtonElement.addEventListener('click', function (e) {
+            let ulSentEmailsElement = document.getElementsByClassName('sent-list')[0];
+
+            let liEl = generateElement('li', '', ulSentEmailsElement);
+            let data = e.currentTarget.parentNode.parentNode.querySelectorAll('h4');
+            let title = data[0].textContent.split(' ')[1];
+            let receiver = data[1].textContent.split(' ')[2];
+
+            let spanEl = generateElement('span', `To: ${receiver}`, liEl);
+            let secondSpanEl = generateElement('span', `Title: ${title}`, liEl);
+
+            let divEl = generateElement('div', '', liEl);
+            divEl.classList.add('btn');
+
+            let buttonEl = generateElement('button', 'Delete', divEl);
+            buttonEl.type = 'submit';
+            buttonEl.classList.add('delete');
+
+            buttonEl.addEventListener('click', function(e) {
+                let currentLiEl = e.currentTarget.parentNode.parentNode;
+
+                let receiverInfo = currentLiEl.querySelectorAll('span')[0].textContent;
+                let titleInfo = currentLiEl.querySelectorAll('span')[1].textContent;
+
+                let liDeleteEl = generateElement('li');
+                let receiverSpanEl = generateElement('span', `To: ${receiverInfo}`, liDeleteEl);
+                let titleSpanEl = generateElement('span', `Title: ${titleInfo}`, liDeleteEl);
     
-                let divEl = generateElement('div', '', liEl);
-                divEl.classList.add(btn);
-    
-                let buttonEl = generateElement('button', 'Delete', divEl);
-                buttonEl.type = 'submit';
-                buttonEl.classList.add('delete');
-
-                let anotherDeleteButtons = Array.from(document.querySelectorAll('.delete'));
-                console.log(anotherDeleteButtons);
-                anotherDeleteButtons.forEach(x => x.addEventListener('click', function(e) {
-                    deleteMail(e);
-                }));
-
                 e.currentTarget.parentNode.parentNode.remove();
-            }
-        }));
+                deleteListEl.appendChild(liDeleteEl);
+            })
+
+            e.currentTarget.parentNode.parentNode.remove();
+
+        });
 
         clearInputs(recipentNameElement, titleElement, textArea);
     });
@@ -86,13 +100,9 @@ function solve() {
     }
 
     function clearInputs() {
-        textArea.value = '';            
+        textArea.value = '';
         titleElement.value = '';
         recipentNameElement.value = '';
     }
-
-    function deleteMail(event) {
-        console.log(1231231231);
-    }
 }
-solve()
+solve();
